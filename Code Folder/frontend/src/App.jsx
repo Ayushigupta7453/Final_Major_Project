034d8f38af7e3d3cbf9df75f9b24a1ef5726f592
Rachit Goyal <rg410345@gmail.com>
2025-04-28 18:11:59 +0530

    feat: tableu integrated within dashboard, ui improvements

--- Code Folder/frontend/src/App.jsx
+++ Code Folder/frontend/src/App.jsx
@@ -35,6 +35,8 @@ function App() {
         <Route path="/login"      element={<Login />} />
         <Route path="/register"   element={<Register />} />
 
+        {/* <Route path="/dashboard" element={<Dashboard/>}/> */}
+
         <Route
           path="/dashboard"
           element={
